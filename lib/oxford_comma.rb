d60e6eb52d99bdc6ab49b07fa4416a1d15d9b636
Randy Steele <steelerx155@gmail.com>
2019-10-16 20:06:57 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
+require 'pry'
 def oxford_comma(array)
-
-end
+   if array.length == 1 
+    "kiwi" 
+ elsif array.length == 2 
+ array.join(" and ")
+ elsif array.length >= 3 
+ durian = "and #{array[-1]}"
+ array.pop
+ array.push(durian)
+# binding.pry
+ array.join(", ")
+  end 
+end 
