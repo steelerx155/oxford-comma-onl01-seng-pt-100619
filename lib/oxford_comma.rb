6415694b01039f7a3ef5f88959b46f4018ea9ea1
Randy Steele <steelerx155@gmail.com>
2019-11-01 13:31:22 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,6 @@
 require 'pry'
 def oxford_comma(array)
+<<<<<<< HEAD
    if array.length == 1 
     "kiwi" 
  elsif array.length == 2 
@@ -11,4 +12,18 @@ def oxford_comma(array)
 # binding.pry
  array.join(", ")
   end 
+=======
+  "kiwi"
+  if array.length == 1 
+    return "kiwi"
+ elsif array.length == 2 
+ array.join(" and ")
+ elsif array.length >= 3 
+ "durian"
+ array.pop
+ array.push(durian)
+# binding.pry
+ array.join(",")
+ end 
+>>>>>>> e40f1743300864c2b86ac9b31080a9e253b38b7c
 end 
